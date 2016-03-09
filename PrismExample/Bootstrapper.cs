e220bca90a14402a2170460ad7e51696322a80f4
Taha Yasin Küçük <webwarrior@hotmail.com>
2016-03-09 20:11:09 +0200

    Prism Sample app first commit

---
+++ PrismExample/Bootstrapper.cs
@@ -0,0 +1,33 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+
+namespace PrismExample
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override IUnityContainer CreateContainer()
+        {
+            return new UnityContainer();
+        }
+
+        protected override DependencyObject CreateShell()
+        {
+            var shell = Container.Resolve<Shell>();
+            return shell;
+        }
+
+        protected override void InitializeShell()
+        {
+            //MainWindow'u shell'e atıyoruz. Ama burada göstermeyeceğiz. 
+            //Çünkü ilk etapta kullanıcı login ekranını görecek. Eğer login başarılıysa
+            //App.xaml.cs içerisinde MainWindow.Show() diyeceğiz.
+            Application.Current.MainWindow = (Window)Shell;
+        }
+    }
+}
